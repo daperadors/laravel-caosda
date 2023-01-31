f006b096625973a05b2bc4cfd005707921903b83
carlos <carlos@caosda.es>
2023-01-31 18:48:30 +0100

    seeders

--- database/seeders/EstudisSeeder.php
+++ database/seeders/EstudisSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EstudisSeeder extends Seeder
 {
@@ -14,6 +15,14 @@ class EstudisSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('estudis')->insert([
+           'nom' => 'DAMVI'
+        ]);
+        DB::table('estudis')->insert([
+            'nom' => 'SMX'
+        ]);
+        DB::table('estudis')->insert([
+            'nom' => 'ASIX'
+        ]);
     }
 }
